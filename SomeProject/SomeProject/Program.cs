919a3829f2d803e13ce84b771f4caa4930eb405f
Edward Arreola <earreola@logitech.com>
2018-08-07 17:55:07 -0700

    added

--- SomeProject/SomeProject/Program.cs
+++ SomeProject/SomeProject/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿//******************************************************************************************************************************
+//  Copyright (c) 1999-2017 Logitech, Inc.  All Rights Reserved
+//
+//  This program is a trade secret of LOGITECH, and it is not to be reproduced,
+//  published, disclosed to others, copied, adapted, distributed or displayed
+//  without the prior authorization of LOGITECH.
+//
+//  Licensee agrees to attach or embbed this notice on all copies of the program,
+//  including partial copies or modified versions thereof.
+//
+//  Description:
+//
+//  Created by Eduardo Arreola
+//******************************************************************************************************************************
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
